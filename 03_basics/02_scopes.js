aa6750c62b646cc3107ab198b158a5b74ddb0e6f
tarun <tarunmalviya320@gmail.com>
2024-09-23 12:53:34 +0530

    why ! use var datatype

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,19 @@
-//var c = 300
+
 let a = 300
 if (true) {
     let a = 10
     const b = 20
+    //var c = 300        // does not follow scope and also can redined with same name
+
     // console.log("INNER: ", a);
     
 }
 
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(a);   // a= 300 
+// console.log(b);   // b is not defined 
+// console.log(c);   // can access value c here also 
 
 
 function one(){
@@ -46,13 +48,13 @@ if (true) {
 
 console.log(addone(5))
 
-function addone(num){
+function addone(num){         // this type of function can defined after calling 
     return num + 1
 }
 
 
 
-addTwo(5)
-const addTwo = function(num){
+addTwo(5)  //Cannot access 'addTwo' before initialization
+const addTwo = function(num){   // but this must defined before call the function 
     return num + 2
 }
