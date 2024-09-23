9545e21fb570ec7f76bac0d1776cc456a1984dd7
tarun <tarunmalviya320@gmail.com>
2024-09-23 17:51:51 +0530

    filter attribute of array

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -10,10 +10,12 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {
-//     return num > 4
-// } )
+const newNums = myNums.filter( (num) => {
+    return num > 4
+} )
+console.log(newNums)     //[ 5, 6, 7, 8, 9, 10 ]
 
+// alternative way with for each and if
 // const newNums = []
 
 // myNums.forEach( (num) => {
