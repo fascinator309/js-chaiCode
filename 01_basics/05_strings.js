4721e0eaf73016fdf23d8847daccc041c35bf6a3
tarun <tarunmalviya320@gmail.com>
2024-09-23 18:19:49 +0530

    String manupulation

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,12 +3,14 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);    //string interpolation
 
 const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName.__proto__);     // {} object
+
+// **********************************String manupulation*********************************
 
 
 // console.log(gameName.length);
@@ -19,17 +21,19 @@ console.log(gameName.indexOf('t'));
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)       // if first index not defined , then default first index is 0
 console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());        // remove extra spaces in front and back
+// also trimStart() or trimEnd().
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))     // check for a word in string , return bool
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));      //[ 'hitesh', 'hc', 'com' ]
+// split string and make array
