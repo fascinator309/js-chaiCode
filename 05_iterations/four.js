efebdfbc523abb63dd7beee223141d6c58b08aa1
tarun <tarunmalviya320@gmail.com>
2024-09-23 17:44:56 +0530

    for-in

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,8 +5,9 @@ const myObject = {
     swift: "swift by apple"
 }
 
+// # for-in loop      -> return key of object , array ,...
 for (const key in myObject) {
-    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
@@ -15,12 +16,20 @@ for (const key in programming) {
     //console.log(programming[key]);
 }
 
-// const map = new Map()
-// map.set('IN', "India")
-// map.set('USA', "United States of America")
-// map.set('Fr', "France")
-// map.set('IN', "India")
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
 
-// for (const key in map) {
-//     console.log(key);
-// }
+console.log(map)
+console.log("line breaker")
+
+for (const key in map) {
+    console.log(key , "forin loop");    // doesn't log any value
+}
+//The issue with your for...in loop not logging any values comes from the fact that a Map in JavaScript is not enumerable in the same way 
+console.log("line breaker")
+for (const key of map) {    // use for-of 
+    console.log(key);
+}
