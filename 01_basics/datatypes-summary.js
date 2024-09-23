73900b60c73b691bc20080216a6afb54cc093a83
tarun <tarunmalviya320@gmail.com>
2024-09-23 19:09:44 +0530

    Symbol datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);    // false because Symbol of same parameter is different
 
 // const bigNumber = 3456543576654356754n
 
