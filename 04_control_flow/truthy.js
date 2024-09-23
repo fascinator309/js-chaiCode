e67f337939a377352b720c6e726d89cdc0f49683
tarun <tarunmalviya320@gmail.com>
2024-09-23 16:20:13 +0530

    Nullish Coalescing Operator (??)

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -13,27 +13,27 @@ if (userEmail) {
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
 
+// why to check array is empty or not
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
 
 const emptyObj = {}
-
+// why to check object is empty or not
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
-// Nullish Coalescing Operator (??): null undefined
+// Nullish Coalescing Operator (??):-  
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// val1 = 5 ?? 10   //take 5
+// val1 = null ?? 10   // take 10
+// val1 = undefined ?? 15  // take 15
+val1 = null ?? 10 ?? 20           // pick first defined value , take 10
 
+console.log("val1: ",val1);
 
-
-console.log(val1);
 
 // Terniary Operator
 
