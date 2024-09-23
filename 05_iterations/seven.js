3ba1ed2c9f4cffc7499c1d46725b25a6f7d1a411
tarun <tarunmalviya320@gmail.com>
2024-09-23 23:14:13 +0530

    Array.map() method

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,21 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+//The map() method of Array instances creates a new array populated with the results of calling a provided function on every element in the calling array.
 
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+//  this is different from Map(object)
+// this is used lke filter methods of array
+
+const newNums = myNumers.map( (num) => { return num + 10})
 
 console.log(newNums);
+
+console.log("line breaker")
+
+
+// chaining of methods
+const newNums2 = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)             // multiple methods using '.' operator
+                .filter( (num) => num >= 40);
+
+console.log(newNums2);
