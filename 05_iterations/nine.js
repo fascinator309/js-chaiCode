f05ba0654a529effdd51dd181e9f3c0d10bcbd34
tarun <tarunmalviya320@gmail.com>
2024-09-23 23:24:29 +0530

    array.reduce() method

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,11 +1,13 @@
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
+// const myTotal = myNums.reduce(function (accumulator, currentValue) {
+//     console.log(`accumulator: ${accumulator} and currentValue: ${currentValue}`);
+//     return accumulator + currentValue
 // }, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+//  0 is the inital value of accumulator
+
+const myTotal = myNums.reduce( (accumulator, currentValue) => accumulator + currentValue, 0)
 
 console.log(myTotal);
 
